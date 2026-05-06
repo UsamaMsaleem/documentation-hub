1ec0044b300fd3e9d9662256cda8d7d627f4623c
UsamaMsaleem <usama.m.saleem98@gmail.com>
2026-05-06 12:11:20 +0500

    Sharing feature add

--- src/models/Category.js
+++ src/models/Category.js
@@ -9,6 +9,7 @@ const categorySchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Workspace',
         required: true
+        
     },
 
 }, { timestamps: true });
